147b972a94d2ee6bb97238a680342393ede3b536
MargaritaChorniak <chorniak.rita@gmail.com>
2023-07-12 20:40:35 +0300

    add optimal strategy and button restart

--- script.js
+++ script.js
@@ -1,56 +1,59 @@
- let matchesLeft = 25;
- let currentPlayer = "player";
+let matchesLeft = 25;
+let currentPlayer = "player";
 
- // Function to update the game board
- function updateGameBoard() {
-     const matchesContainer = document.getElementById("matches-container");
-     matchesContainer.innerHTML = "";
-     for (let i = 0; i < matchesLeft; i++) {
-         const match = document.createElement("div");
-         match.className = "match";
-         matchesContainer.appendChild(match);
-     }
+function updateGameBoard() {
+    const matchesContainer = document.getElementById("matches-container");
+    matchesContainer.innerHTML = "";
+    for (let i = 0; i < matchesLeft; i++) {
+        const match = document.createElement("div");
+        match.className = "match";
+        matchesContainer.appendChild(match);
+    }
+}
+
+function switchTurn() {
+    currentPlayer = currentPlayer === "player" ? "AI" : "player";
+    const playerTurn = document.getElementById("player-turn");
+    playerTurn.style.display = currentPlayer === "player" ? "block" : "none";
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent = "";
+    if (currentPlayer === "AI") {
+        setTimeout(makeAITurn, 3000);
+        resultDiv.textContent = `AI takes matches`;
+    }
+}
+
+function takeMatches(num) {
+    if (num >= 1 && num <= 3 && matchesLeft - num >= 0) {
+        matchesLeft -= num;
+        updateGameBoard();
+        if (matchesLeft === 0) {
+            showResult();
+        } else {
+            switchTurn();
+        }
+    }
+}
+
+function makeAITurn() {
+  let aiNum;
+  if ((matchesLeft - 1) % 4 === 0) {
+    aiNum = 1;
+  } else if ((matchesLeft - 2) % 4 === 0) {
+    aiNum = 2;
+  } else {
+    aiNum = 3;
+  };
+
+  matchesLeft -= aiNum;
+  updateGameBoard();
+  if (matchesLeft === 0) {
+    showResult();
+  } else {
+    switchTurn();
+  }
  }
 
- // Function to switch the player turn
- function switchTurn() {
-     currentPlayer = currentPlayer === "player" ? "AI" : "player";
-     const playerTurn = document.getElementById("player-turn");
-     playerTurn.style.display = currentPlayer === "player" ? "block" : "none";
-     const resultDiv = document.getElementById("result");
-     resultDiv.textContent = "";
-     if (currentPlayer === "AI") {
-         setTimeout(makeAITurn, 3000);
-         resultDiv.textContent = "AI turn...";
-     }
- }
-
- // Function to handle player turn
- function takeMatches(num) {
-     if (num >= 1 && num <= 3 && matchesLeft - num >= 0) {
-         matchesLeft -= num;
-         updateGameBoard();
-         if (matchesLeft === 0) {
-             showResult();
-         } else {
-             switchTurn();
-         }
-     }
- }
-
- // Function to handle AI turn
- function makeAITurn() {
-     const aiNum = Math.floor(Math.random() * 3) + 1;
-     matchesLeft -= aiNum;
-     updateGameBoard();
-     if (matchesLeft === 0) {
-         showResult();
-     } else {
-         switchTurn();
-     }
- }
-
- // Function to display game result
  function showResult() {
      const resultDiv = document.getElementById("result");
      if (matchesLeft % 2 === 0) {
@@ -58,8 +61,12 @@
      } else {
          resultDiv.textContent = currentPlayer === "player" ? "AI Wins!" : "You Win!";
      }
-     
  }
 
- // Initialize the game board
+ function restartGame() {
+  matchesLeft = 25;
+  isPlayerTurn = true;
+  updateGameBoard();
+}
+
  updateGameBoard();
